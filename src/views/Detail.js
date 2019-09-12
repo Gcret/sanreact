c764f6f5591293f7b52a45cad44f84cbf709fd7a
guozhidi <15093738256@163.com>
2019-09-12 11:48:35 +0800

    new

--- src/views/Detail.js
+++ src/views/Detail.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import {NavLink} from 'react-router-dom';
-import { Input, Button,Icon,Menu,Dropdown, Carousel,BackTop} from 'antd';
+import { Input, Button,Icon,Menu,Dropdown, Carousel,BackTop,Select , Calendar} from 'antd';
+
 import style from './Detail.module.css';
 
 
@@ -24,8 +25,34 @@ const menu = (
     </Menu.Item>
   </Menu>
 );
+const { Option } = Select;
+	function onChange(value) {
+  //console.log(`selected ${value}`);
+}
 
-export default class Detail extends Component{
+function onBlur() {
+  //console.log('blur');
+}
+
+function onFocus() {
+  //console.log('focus');
+}
+
+function onSearch(val) {
+  //console.log('search:', val);
+}	
+const ButtonGroup = Button.Group;
+		
+function onPanelChange(value, mode) {
+  //console.log(value, mode);
+}		
+		
+		
+		
+		
+		
+		
+	export default class Ariben extends Component{
 	render(){
 		return(
 			<div>
@@ -121,8 +148,75 @@ export default class Detail extends Component{
 									<span className={style.biaot}>标题</span>
 									<img src=" " className={style.imgs1}/>
 									
-									<p className={style.chanpin}>产品编码：00010906</p>
-									<div className={style.chufa}><Icon type="environment" />&nbsp;出发地:北京</div>
+									<p className={style.chanpin}>产品编码：00010906</p><br/>
+									<div className={style.chufa}><Icon className={style.chufa1} type="environment" />&nbsp;出发地:北京</div>
+									
+									<div className={style.jiage}>
+												<span className={style.jiage1}>￥</span><span className={style.jiage2}>{8688}</span><span className={style.jiage3}>起/人</span>	<span className={style.jiage4}>已省￥300</span>
+									</div>
+									
+									<div className={style.youhui}><span className={style.youhui1}>优惠促销:</span><span className={style.youhui2}>金秋放假  乐享满减</span></div>
+									
+									<div className={style.xuanze}>
+													<div className={style.xuanze1}>
+															<span className={style.xuanze2}>选择团期:</span>
+																<span className={style.xuanze3}>
+																   <Select
+																	    showSearch
+																	    style={{ width: 450 }}
+																	    placeholder="请选择出团日期"
+																	    optionFilterProp="children"
+																	    onChange={onChange}
+																	    onFocus={onFocus}
+																	    onBlur={onBlur}
+																	    onSearch={onSearch}
+																	    filterOption={(input, option) =>
+																	      option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+																	    }
+																	  >
+																	    <Option value="jack">11-05(周二)出发 - 8699/人 - 已减¥300 - A行</Option>
+																	    <Option value="lucy">11-21(周四)出发 - 8699/人 - 已减¥300 - A行程</Option>
+																	    <Option value="tom">12-03(周二)出发 - 8699/人 - 已减¥300 - A行程</Option>
+																	  </Select>
+																	</span>
+													</div>
+													
+														<div className={style.xuanze4}>
+																	<span className={style.xuanze5}>选择人数:</span>
+																	
+																			 <ButtonGroup className={style.xuanze6}>
+																		      <Button className={style.xuanze7} onClick={this.jia.bind(this)}><Icon type="minus" /></Button>
+																		      <Input placeholder="人数"  className={style.xuanze8} />
+																		      <Button className={style.xuanze9} onClick={this.jian.bind(this)}><Icon type="plus" /></Button>
+																		    </ButtonGroup>
+																		<Button className={style.btns9} onClick={this.tijiao.bind(this)}>立即预定</Button>
+														</div>
+											
+											
+											<div className={style.rili}>
+													<div style={{ width: 400, border: '1px solid #d9d9d9', borderRadius: 4 }}>
+												    <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+												  </div>											
+											</div>
+											
+											<div className={style.jieshao}>
+													<p>
+															<b>行程特色</b><br/>
+															艺术典藏：大英博物馆是鉴赏世界*艺术珍品走进世界上规模*、**的博物馆；<br/>
+															名校之城：牛津沉醉在历史的情怀里<br/>	
+													</p>
+													<p>
+														伦敦符号： 从大本钟到白金汉宫，走进大英博物馆令古老的伦敦更显深邃之意，前往乔治风格建筑物唐宁街10号；<br/>
+														自悠自在：伦敦全天自由活动，尽享伦敦的时尚与无穷魅力，比斯特乐享购物，感受超低价格的超值体验；
+													</p>
+													<p>
+															接口数据
+													</p>
+											</div>
+											
+											
+											
+									</div>
 						</div>
 				
 				
@@ -169,4 +263,14 @@ export default class Detail extends Component{
 			</div>
 		)
 	}
+		
+		jia(){
+		
+		}
+		jian(){
+		
+		}
+		tijiao(){
+			
+		}
 }
